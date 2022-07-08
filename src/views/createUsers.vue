2a441a24484281ed84ce02014ccb9ca93696c218
AlexVL19 <avalencia190@misena.edu.co>
2022-07-08 17:47:52 -0500

    Creación de aplicación, vista de usuarios en proceso

---
+++ src/views/createUsers.vue
@@ -0,0 +1,60 @@
+<template>
+<div class="createUsers">
+    <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+</div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+    data: () => ({
+        usuario: null
+    }),
+
+    created() {
+        const api = 'http://localhost:8000/api/users/'
+
+        axios.get(api)
+        .then((response) => {
+            console.log(response.data);
+            this.usuario = response.data;
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+}
+</script>
