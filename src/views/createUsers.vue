e137bb7def6f51e71fdc7b8354f86d8a815dcb45
AlexVL19 <avalencia190@misena.edu.co>
2022-07-09 11:45:57 -0500

    Creación de una lista para mostrar usuarios. Tabla de datos en proceso

--- src/views/createUsers.vue
+++ src/views/createUsers.vue
@@ -1,35 +1,16 @@
 <template>
 <div class="createUsers">
-    <table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+<ol>
+  <li v-for="usu in usuario" v-bind:key="usu.id">
+  {{usu.name}}
+  </li>
+</ol>
+
+<!--<v-app>
+  <v-data-table
+    :usuario="usuario">
+  </v-data-table>
+</v-app>-->
 
 </div>
 </template>
